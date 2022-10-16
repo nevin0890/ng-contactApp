7a3a40589dab69d10d0cc7b2ad1d08c37da8ae44
nevin0890 <bcab19_2217@santhigiricollege.com>
2022-10-16 18:43:10 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{HttpClientModule} from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
